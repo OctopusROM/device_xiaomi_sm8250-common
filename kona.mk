7b8286bf321de8c7ac1256a183bad1e4b9f978d6
LittleChest <hiya@littlew.top>
2024-12-15 09:18:09 +0800

    Revert "sm8250-common: Remove common QTI vibrator HAL"
    This reverts commit eef7d2047237100af976b8e9879075c4220c761b.

--- kona.mk
+++ kona.mk
@@ -546,6 +546,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
