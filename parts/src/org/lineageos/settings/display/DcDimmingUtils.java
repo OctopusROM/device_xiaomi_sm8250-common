80ef86de50d29558227768e713fbaa6f052b3198
ushion0a <1.next632@passfwd.com>
2026-09-25 02:51:33 +0800

    sm8250-common: add DC dimming sysfs helper
    Change-Id: I11fc74562657ff6cdaf3470eb59d815f0bfc6b67

---
+++ parts/src/org/lineageos/settings/display/DcDimmingUtils.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2026 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.display;
+
+import org.lineageos.settings.utils.FileUtils;
+
+public final class DcDimmingUtils {
+    public static final String KEY = "dc_dimming_enable";
+    public static final String NODE =
+            "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/msm_fb_ea_enable";
+
+    private DcDimmingUtils() {
+    }
+
+    public static boolean isAvailable() {
+        return FileUtils.isFileWritable(NODE);
+    }
+
+    public static boolean isEnabled() {
+        return "1".equals(FileUtils.readOneLine(NODE));
+    }
+
+    public static boolean setEnabled(boolean enabled) {
+        return FileUtils.writeLine(NODE, enabled ? "1" : "0");
+    }
+}
